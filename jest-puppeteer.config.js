762258b8040df881ddfe3aa35de39a7796bd5c4d
Alfred Madere <alfred@stevemadere.com>
2020-04-30 13:47:50 -0500

    first commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+//jest-puppeteer.config.js
+module.exports = {
+  launch: {
+    headless: false, 
+    devtools: false,
+    slowMo: 100
+  }
+};
---
+++ smoke.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const faker = require('faker');
+
+
+const user = {
+	email: faker.internet.email(),
+	firstName: faker.name.firstName(),
+	lastName: faker.name.lastName(),
+	mobile: faker.phone.phoneNumber(),
+	companyName: faker.company.companyName(),
+	textBlock: 'i need some big info',
+	password: faker.internet.password(),
+
+};
+
+
+describe('jamkazam', () => {
+	jest.setTimeout(30000);
+  beforeEach(async () => {
+  	const app = 'https://www.jamkazam.com/';
+    await page.goto(app);
+  });
+
+//only pases in non headless mode
+
+  it('can visit jamkazam.com', async() => {
+    page.waitForSelector('a.join-today');
+  });
+
+});
+
